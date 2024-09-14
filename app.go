d643c5b18d47e925586c5fe6af71d14835c3d5d5
Aapo Attila <aapo.attila@windowslive.com>
2024-09-14 18:24:08 +0300

    REPL finished

--- app.go
+++ app.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"monke/repl"
+)
 
 func main() {
-	fmt.Print("Hello monke")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monke programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
